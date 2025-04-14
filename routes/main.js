1aa437c2893372c0378220c27ac2486e72431af5
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-14 10:11:40 +0100

    added search path and search form

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/", (req, res) => res.render("index.ejs", {title: "Dynamic Title", header: "This is the Homepage"}));
 router.get("/about", (req, res) => res.render("about.ejs", {aboutTitle: "About", aboutHeader: "This is the about page"}));
 router.get("/contact", (req, res) => res.render("contact.ejs"))
+router.get("/search", (req, res) => res.render("search.ejs"))
 
 // Export the router object
 module.exports = router
