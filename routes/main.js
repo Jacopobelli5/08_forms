5846b684ddf37350fab4dfcf37aa1531c4b834ab
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-14 10:07:09 +0100

    init

---
+++ routes/main.js
@@ -0,0 +1,11 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+// Handle the main routes
+router.get("/", (req, res) => res.render("index.ejs", {title: "Dynamic Title", header: "This is the Homepage"}));
+router.get("/about", (req, res) => res.render("about.ejs", {aboutTitle: "About", aboutHeader: "This is the about page"}));
+router.get("/contact", (req, res) => res.render("contact.ejs"))
+
+// Export the router object
+module.exports = router
